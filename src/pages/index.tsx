1af886d1b15b7eab151cc1ec0d7307674a60c1f0
radioDevCreations <radekrozmus@gmail.com>
2021-09-12 18:22:31 +0200

    normalize.css added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,12 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import SEO from "../components/SEO"
+import Layout from "../components/Layout"
 const IndexPage = () => {
   return (
-    <>
+    <Layout>
       <h1>Gatsby Strapi Portfolio Starter</h1>
-    </>
+    </Layout>
   )
 }
 
